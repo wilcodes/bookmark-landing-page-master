1e90a9f369ecefbb22c8f386225f322f578a2983
Wilfredo R Colina <wrca26@gmail.com>
2022-10-21 11:51:56 -0500

    Fixing navigation

--- hamburger.js
+++ hamburger.js
@@ -2,17 +2,39 @@ var hamburger = document.querySelector('.hamburger');
 let isModalOpen = false;
 
 const handleHamburger = ()=>{
+
+    
    
      if (!isModalOpen) {
           document.querySelector(".hamburger-modal").style.display = "block";
+          document.querySelector("ion-icon").style.color = "#fff";
+          document.querySelector(".white-logo").style.display = "block";
+          document.querySelector(".logo").style.display = "none";
           
         } else {
           document.querySelector(".hamburger-modal").style.display = "none";
+          document.querySelector("ion-icon").style.color = "#000";
+          document.querySelector(".logo").style.display = "block";
+          document.querySelector(".white-logo").style.display = "none";
         }
         isModalOpen = !isModalOpen;     
 
 
 };
+
+window.addEventListener('resize',()=>{
+  if(window.innerWidth > 600){
+    document.querySelector(".hamburger-modal").style.display = "none";
+    document.querySelector(".logo").style.display = "block";
+    document.querySelector("ion-icon").style.color = "#000";
+    document.querySelector(".white-logo").style.display = "none";
+  }
+})
+
+window
+
+
+
 
 
 
